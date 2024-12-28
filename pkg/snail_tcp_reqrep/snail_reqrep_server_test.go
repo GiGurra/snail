88de3a04fb788daacea4bc732b8eedd9245b1f66
Johan Kjölhede <johan.kjolhede@gmail.com>
2024-12-28 10:10:29 +0100

    wip

--- pkg/snail_tcp_reqrep/snail_reqrep_server_test.go
+++ pkg/snail_tcp_reqrep/snail_reqrep_server_test.go
@@ -1,13 +1,79 @@
 package snail_tcp_reqrep
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/GiGurra/snail/pkg/snail_logging"
+	"github.com/GiGurra/snail/pkg/snail_parser"
 	"log/slog"
+	"net"
 	"testing"
 )
+
+type requestStruct struct {
+	Msg string
+}
+
+type responseStruct struct {
+	Msg string
+}
 
 func TestNewServer_SendAndRespondWithJson(t *testing.T) {
 	snail_logging.ConfigureDefaultLogger("text", "info", false)
 
 	slog.Info("TestNewServer_SendAndRespondWithJson")
+
+	reqCodec := snail_parser.NewJsonLinesCodec[requestStruct]()
+	respCodec := snail_parser.NewJsonLinesCodec[responseStruct]()
+
+	server, err := NewServer[requestStruct, responseStruct](
+		func() ServerConnHandler[requestStruct, responseStruct] {
+			return func(req *requestStruct, repFunc func(resp *responseStruct) error) error {
+				slog.Info("Server received request", slog.String("msg", req.Msg))
+				return repFunc(&responseStruct{Msg: "Hello from server"})
+			}
+		},
+		nil,
+		reqCodec.Parser,
+		respCodec.Writer,
+	)
+
+	if err != nil {
+		t.Fatalf("error creating server: %v", err)
+	}
+
+	defer server.Close()
+
+	if server == nil {
+		t.Fatalf("expected server, got nil")
+	}
+
+	// Open a socket to the server and send a request
+	sock, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", server.Underlying().Port()))
+	if err != nil {
+		t.Fatalf("error dialing server: %v", err)
+	}
+
+	// Send a request
+	_, err = sock.Write([]byte(`{"Msg":"Hello from client"}` + "\n"))
+	if err != nil {
+		t.Fatalf("error sending request: %v", err)
+	}
+
+	// Read the response
+	buf := make([]byte, 1024)
+	n, err := sock.Read(buf)
+	if err != nil {
+		t.Fatalf("error reading response: %v", err)
+	}
+
+	resp := responseStruct{}
+	err = json.Unmarshal(buf[:n], &resp)
+	if err != nil {
+		t.Fatalf("error unmarshalling response: %v", err)
+	}
+
+	if resp.Msg != "Hello from server" {
+		t.Fatalf("expected response 'Hello from server', got '%s'", resp.Msg)
+	}
 }
