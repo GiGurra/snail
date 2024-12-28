4e14fd4f7edd18ef2860edcc4a18a8b03be0c602
Johan Kjölhede <johan.kjolhede@gmail.com>
2024-12-28 15:55:34 +0100

    wip

--- pkg/snail_tcp_reqrep/snail_reqrep_client_test.go
+++ pkg/snail_tcp_reqrep/snail_reqrep_client_test.go
@@ -88,3 +88,94 @@ func TestNewClient_SendAndRespondWithJson(t *testing.T) {
 	}
 
 }
+
+func TestNewClient_SendAndRespondWithJson_1s_naive_performance(t *testing.T) {
+	snail_logging.ConfigureDefaultLogger("text", "info", false)
+
+	slog.Info("TestNewServer_SendAndRespondWithJson")
+
+	testLength := 1 * time.Second
+
+	reqCodec := snail_parser.NewJsonLinesCodec[*requestStruct]()
+	respCodec := snail_parser.NewJsonLinesCodec[*responseStruct]()
+
+	server, err := NewServer[*requestStruct, *responseStruct](
+		func() ServerConnHandler[*requestStruct, *responseStruct] {
+			return func(req *requestStruct, repFunc func(resp *responseStruct) error) error {
+
+				if repFunc == nil {
+					slog.Warn("Client disconnected")
+					return nil
+				}
+
+				//slog.Info("Server received request", slog.String("msg", req.Msg))
+				return repFunc(&responseStruct{Msg: "Hello from server"})
+			}
+		},
+		nil,
+		reqCodec.Parser,
+		respCodec.Writer,
+	)
+
+	if err != nil {
+		t.Fatalf("error creating server: %v", err)
+	}
+
+	if server == nil {
+		t.Fatalf("expected server, got nil")
+	}
+
+	defer server.Close()
+
+	responseChan := make(chan *responseStruct)
+
+	respHandler := func(resp *responseStruct) error {
+
+		if resp == nil {
+			slog.Warn("Server disconnected")
+			return nil
+		}
+
+		//slog.Info("Client received response", slog.String("msg", resp.Msg))
+		responseChan <- resp
+		return nil
+	}
+
+	client, err := NewClient[*requestStruct, *responseStruct](
+		"localhost",
+		server.Port(),
+		nil,
+		respHandler,
+		reqCodec.Writer,
+		respCodec.Parser,
+	)
+
+	if err != nil {
+		t.Fatalf("error creating client: %v", err)
+	}
+
+	defer client.Close()
+
+	nReqResps := 0
+	t0 := time.Now()
+	for time.Since(t0) < testLength {
+
+		err = client.Send(&requestStruct{Msg: "Hello from client"})
+		if err != nil {
+			t.Fatalf("error sending request: %v", err)
+		}
+
+		select {
+		case resp := <-responseChan:
+			if resp.Msg != "Hello from server" {
+				t.Fatalf("expected response msg 'Hello from server', got '%s'", resp.Msg)
+			}
+			nReqResps++
+		case <-time.After(1 * time.Second):
+			t.Fatalf("timeout waiting for response")
+		}
+	}
+
+	slog.Info("Sent and received", slog.Int("nReqResps", nReqResps))
+
+}
