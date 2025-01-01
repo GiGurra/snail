f9e0de5b7a7b53313e7feb90485d464ab973a836
Johan Kjölhede <johan.kjolhede@gmail.com>
2025-01-01 20:13:06 +0100

    wip on tooling

--- cmd/cmd_load/cmd_load_h1/cmd_load_h1.go
+++ cmd/cmd_load/cmd_load_h1/cmd_load_h1.go
@@ -1,14 +1,19 @@
 package cmd_load_h1
 
 import (
+	"fmt"
 	"github.com/GiGurra/boa/pkg/boa"
 	"github.com/spf13/cobra"
 	"log/slog"
+	"os"
 )
 
 type Params struct {
-	Connections           boa.Required[int] `help:"Number of connections to use"`
-	MaxConcurrentRequests boa.Required[int] `help:"Number of concurrent requests to use per connection"`
+	Connections           boa.Required[int]    `help:"Number of connections to use"`
+	MaxConcurrentRequests boa.Required[int]    `help:"Number of concurrent requests to use per connection"`
+	Url                   boa.Required[string] `help:"Url to use" positional:"true"`
+	Duration              boa.Optional[int]    `help:"Duration to run the test for in seconds"`
+	NumberOfRequests      boa.Optional[int]    `help:"Number of requests to run"`
 }
 
 func Cmd() *cobra.Command {
@@ -23,7 +28,18 @@ func Cmd() *cobra.Command {
 			boa.ParamEnricherBool,
 		),
 		Run: func(cmd *cobra.Command, args []string) {
-			slog.Info("Hello, World load h1!")
+			if params.Duration.HasValue() && params.NumberOfRequests.HasValue() {
+				exitWithError("Cannot specify both duration and number of requests")
+			}
+
+			fmt.Printf("Will load test %s using:\n", params.Url.Value())
+			fmt.Printf("  %d connection(s)\n", params.Connections.Value())
+			fmt.Printf("  %d concurrent request(s) per connection\n", params.MaxConcurrentRequests.Value())
 		},
 	}.ToCmd()
+}
+
+func exitWithError(msg string) {
+	slog.Error(msg)
+	os.Exit(1)
 }
