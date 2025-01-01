08ced3ba2ec4ea6af8e8388c5371354be3231afb
Johan Kjölhede <johan.kjolhede@gmail.com>
2025-01-01 12:43:53 +0100

    cleanup

--- pkg/snail_test_util/http1srv/main.go
+++ pkg/snail_test_util/http1srv/main.go
@@ -84,45 +84,32 @@ func newHandlerFunc(conn net.Conn) snail_tcp.ServerConnHandler {
 				continue
 			}
 
-			if !state.StartLineReading && !state.StartLineReceived {
-				// First character must be G for GET, the rest is ignored
+			switch state.Status {
+			case LookingForStartLine:
 				if b != 'G' {
 					return fmt.Errorf("unsupported request method: %c", b)
 				}
 				state.StartLineStart = i
-				state.StartLineReading = true
-				continue
-			}
-
-			if state.StartLineReading {
+				state.Status = ReadingStartLine
+			case ReadingStartLine:
 				if b == '\n' {
 					state.StartLine = string(bytes[state.StartLineStart:i])
 					//fmt.Printf("Start line: %s\n", state.StartLine)
-					state.StartLineReceived = true
-					state.StartLineReading = false
-					state.HeadersReading = true
-					continue
+					state.Status = ReadingHeaders
 				}
-			}
-
-			if state.HeadersReading {
+			case ReadingHeaders:
 				if state.CurrentHeaderStart <= 0 {
 					state.CurrentHeaderStart = i
 				}
 				if b == '\n' {
 					// End of header
-					state.HeadersReceived = true
 					headerLen := i - state.CurrentHeaderStart
 					if headerLen == 0 {
 						// End of headers
-						state.HeadersReading = false
-						state.HeadersReceived = true
 						//fmt.Println("End of headers")
-						state.RequestComplete = true
 						responsesToSend++
 						state = getRequestState{}
 						readBuf.SetReadPos(i + 1)
-						continue
 					} else {
 						header := string(bytes[state.CurrentHeaderStart:i])
 						state.Headers = append(state.Headers, header)
@@ -132,7 +119,6 @@ func newHandlerFunc(conn net.Conn) snail_tcp.ServerConnHandler {
 					continue
 				}
 			}
-
 		}
 
 		//fmt.Printf("Responses to send: %d\n", responsesToSend)
@@ -163,18 +149,22 @@ func newHandlerFunc(conn net.Conn) snail_tcp.ServerConnHandler {
 	}
 }
 
-type getRequestState struct {
-	StartLineReading  bool
-	StartLineReceived bool
-	StartLineStart    int
-	StartLine         string
+type Status int
 
-	HeadersReading     bool
-	HeadersReceived    bool
+const (
+	LookingForStartLine Status = iota
+	ReadingStartLine    Status = iota
+	ReadingHeaders
+)
+
+type getRequestState struct {
+	Status Status
+
+	StartLineStart int
+	StartLine      string
+
 	CurrentHeaderStart int
 	Headers            []string
-
-	RequestComplete bool
 }
 
 // StripMarginWith Accepts a string and a `marginChar` rune to strip margins
