a55bdba91e662f82b9e7be79ab0d5d303fde421a
Johan Kjölhede <johan.kjolhede@gmail.com>
2025-01-01 20:43:59 +0100

    wip on tooling

--- cmd/cmd_load/cmd_load_h1/cmd_load_h1.go
+++ cmd/cmd_load/cmd_load_h1/cmd_load_h1.go
@@ -3,20 +3,25 @@ package cmd_load_h1
 import (
 	"fmt"
 	"github.com/GiGurra/boa/pkg/boa"
+	"github.com/GiGurra/snail/pkg/snail_batcher"
+	"github.com/GiGurra/snail/pkg/snail_buffer"
+	"github.com/GiGurra/snail/pkg/snail_tcp"
 	"github.com/GiGurra/snail/pkg/snail_test_util/strutil"
 	"github.com/spf13/cobra"
 	"log/slog"
 	"net/url"
 	"os"
+	"strconv"
 	"time"
 )
 
 type Params struct {
-	Connections           boa.Required[int]    `help:"Number of connections to use"`
-	MaxConcurrentRequests boa.Required[int]    `help:"Number of concurrent requests to use per connection"`
-	Url                   boa.Required[string] `help:"Url to use" positional:"true"`
-	Duration              boa.Optional[string] `help:"Duration to run the test for"`
-	NumberOfRequests      boa.Optional[int]    `help:"Number of requests to run"`
+	Connections           boa.Required[int]    `descr:"Number of connections to use"`
+	MaxConcurrentRequests boa.Required[int]    `descr:"Number of concurrent requests to use per connection"`
+	Url                   boa.Required[string] `descr:"Url to use" positional:"true"`
+	BatchSizeBytes        boa.Required[int]    `descr:"Batch size in bytes for tcp writes" default:"65536"`
+	Duration              boa.Optional[string] `descr:"Duration to run the test for"`
+	NumberOfRequests      boa.Optional[int]    `descr:"Number of requests to run"`
 }
 
 func (p *Params) WithValidation() *Params {
@@ -55,6 +60,12 @@ func (p *Params) WithValidation() *Params {
 		}
 		return nil
 	}
+	p.BatchSizeBytes.CustomValidator = func(i int) error {
+		if i <= 0 {
+			return fmt.Errorf("batch size must be greater than 0")
+		}
+		return nil
+	}
 	return p
 }
 
@@ -86,6 +97,53 @@ func Cmd() *cobra.Command {
 				fmt.Printf("  %s test duration\n", duration)
 			} else if params.NumberOfRequests.HasValue() {
 				fmt.Printf("  %d request(s) total spread across connections\n", *params.NumberOfRequests.Value())
+			}
+
+			respHandler := func(resp *snail_buffer.Buffer) error {
+				fmt.Printf("Received response: %s\n", resp.String())
+				return nil
+			}
+
+			parsedUrl, _ := url.Parse(params.Url.Value())
+			host := parsedUrl.Hostname()
+			port := func() int {
+				if parsedUrl.Port() == "" {
+					return 80
+				}
+				port, err := strconv.Atoi(parsedUrl.Port())
+				if err != nil {
+					exitWithError(fmt.Sprintf("Failed to parse port: %v", err))
+				}
+				return port
+			}()
+
+			slog.Info("Creating clients")
+			clients := make([]*snail_tcp.SnailClient, params.Connections.Value())
+			for i := 0; i < params.Connections.Value(); i++ {
+				var err error
+				clients[i], err = snail_tcp.NewClient(host, port, nil, respHandler)
+				if err != nil {
+					exitWithError(fmt.Sprintf("Failed to create client: %v", err))
+				}
+			}
+
+			slog.Info("Creating batchers")
+			batchers := make([]*snail_batcher.SnailBatcher[byte], params.Connections.Value())
+			for i := 0; i < params.Connections.Value(); i++ {
+				client := clients[i]
+				batchers[i] = snail_batcher.NewSnailBatcher(
+					params.BatchSizeBytes.Value(),
+					params.BatchSizeBytes.Value()*2,
+					false,
+					1*time.Minute,
+					func(bytes []byte) error {
+						err := client.SendBytes(bytes)
+						if err != nil {
+							return fmt.Errorf("failed to send bytes: %v", err)
+						}
+						return nil
+					},
+				)
 			}
 		},
 	}.ToCmd()
