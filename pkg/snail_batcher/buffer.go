832e23f00beab89f18b51b8b36e414ae86b64540
Johan Kjölhede <johan.kjolhede@gmail.com>
2025-01-02 13:44:16 +0100

    wip crazy

---
+++ pkg/snail_batcher/buffer.go
@@ -0,0 +1,62 @@
+package snail_batcher
+
+import (
+	"fmt"
+	"log/slog"
+	"runtime"
+	"sync/atomic"
+	"unsafe"
+)
+
+const CacheLinePadding = 64 // makes it 2-3x faster :D, due to prevention of false sharing
+
+type Buffer struct {
+	_        [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
+	writePos atomic.Uint64
+	_        [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
+	nWritten atomic.Uint64
+	//_        [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
+	//bufsize  int
+	_    [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
+	data []int64
+}
+
+func NewBuffer(size int) *Buffer {
+	return &Buffer{
+		data: make([]int64, size),
+	}
+}
+
+func (b *Buffer) PushOne(item int64) {
+
+tryAgain:
+
+	writePos := b.writePos.Add(1) - 1
+	if int(writePos) == len(b.data) {
+		for b.nWritten.Load() != uint64(len(b.data)) {
+			runtime.Gosched() // must wait until all elements are written, i.e. all other goroutines have finished writing
+		}
+		// we flush
+		slog.Info(fmt.Sprintf("Flushing buffer. nWritten: %d", b.nWritten.Load()))
+		b.nWritten.Store(0)
+		b.writePos.Store(0)
+		goto tryAgain
+	} else if int(writePos) > len(b.data) {
+		runtime.Gosched()
+		goto tryAgain
+	}
+
+	b.data[writePos] = item
+	b.nWritten.Add(1)
+}
+
+// ToPointer Helper function to convert any value to unsafe.Pointer
+func ToPointer[T any](item T) unsafe.Pointer {
+	return unsafe.Pointer(&item)
+}
+
+//go:linkname procPin runtime.procPin
+func procPin() int
+
+//go:linkname procUnpin runtime.procUnpin
+func procUnpin()
