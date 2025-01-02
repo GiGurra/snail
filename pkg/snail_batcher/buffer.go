156c550fe4eccf49f0c86bdc21548c10773a406e
Johan Kjölhede <johan.kjolhede@gmail.com>
2025-01-02 14:20:05 +0100

    wip

--- pkg/snail_batcher/buffer.go
+++ pkg/snail_batcher/buffer.go
@@ -4,24 +4,32 @@ import (
 	"fmt"
 	"log/slog"
 	"runtime"
+	"sync"
 	"sync/atomic"
 )
 
 const CacheLinePadding = 64 // makes it 2-3x faster :D, due to prevention of false sharing
 
 type Buffer[T any] struct {
-	_        [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
-	pushChan chan []int64
-	_        [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
-	pullChan chan []int64
-	_        [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
-	writePos atomic.Uint64
-	_        [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
-	nWritten atomic.Uint64
+	_             [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
+	pullChanMutex sync.Mutex
+	_             [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
+	pushChan      chan flushingBatch[T]
+	_             [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
+	pullChan      chan []T
+	_             [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
+	writePos      atomic.Uint64
+	_             [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
+	nWritten      atomic.Uint64
 	//_        [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
 	//bufsize  int
 	_    [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
 	data []T
+}
+
+type flushingBatch[T any] struct {
+	size int
+	buf  []T
 }
 
 func NewBuffer[T any](batchSize int, queueSize int) *Buffer[T] {
@@ -34,8 +42,40 @@ func NewBuffer[T any](batchSize int, queueSize int) *Buffer[T] {
 		panic("queueSize must be a multiple of batchSize")
 	}
 
-	return &Buffer[T]{
-		data: make([]T, batchSize),
+	totalBufCount := queueSize/batchSize + 1
+
+	res := &Buffer[T]{
+		pushChan: make(chan flushingBatch[T], totalBufCount),
+		pullChan: make(chan []T, totalBufCount),
+		data:     make([]T, batchSize),
+	}
+
+	// add totalBufCount to pullChan
+	for i := 0; i < totalBufCount; i++ {
+		res.pullChan <- make([]T, batchSize)
+	}
+
+	go res.workerLoop()
+
+	return res
+}
+
+func (b *Buffer[T]) Close() {
+	close(b.pushChan)
+}
+
+func (b *Buffer[T]) workerLoop() {
+
+	// TODO: Create ticker to flush buffer every x seconds
+
+	for {
+		select {
+		case buf := <-b.pushChan:
+			// TODO: Determine read length
+			slog.Info(fmt.Sprintf("Flushing buffer. nWritten: %d", buf.size))
+			// push it back
+			b.pullChan <- buf.buf
+		}
 	}
 }
 
@@ -43,12 +83,23 @@ func (b *Buffer[T]) PushOne(item T) {
 
 tryAgain:
 
+	if b.data == nil {
+		func() {
+			b.pullChanMutex.Lock()
+			defer b.pullChanMutex.Unlock()
+			if b.data == nil {
+				b.data = <-b.pullChan
+			}
+		}()
+	}
+
 	writePos := b.writePos.Add(1) - 1
 	if int(writePos) == len(b.data) {
 		for b.nWritten.Load() != uint64(len(b.data)) {
 			runtime.Gosched() // must wait until all elements are written, i.e. all other goroutines have finished writing
 		}
-		slog.Info(fmt.Sprintf("Flushing buffer. nWritten: %d", b.nWritten.Load()))
+		b.pushChan <- flushingBatch[T]{size: len(b.data), buf: b.data}
+		b.data = nil
 		b.nWritten.Store(0)
 		b.writePos.Store(0)
 		goto tryAgain
