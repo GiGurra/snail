4fa61455624a24976189c066cd12b633b11dd869
Johan Kjölhede <johan.kjolhede@gmail.com>
2025-01-02 14:34:21 +0100

    wip

--- pkg/snail_batcher/buffer.go
+++ pkg/snail_batcher/buffer.go
@@ -10,6 +10,15 @@ import (
 
 const CacheLinePadding = 64 // makes it 2-3x faster :D, due to prevention of false sharing
 
+type proccessingBatch[T any] struct {
+	_        [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
+	writePos atomic.Uint64
+	_        [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
+	nWritten atomic.Uint64
+	_        [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
+	data     []T
+}
+
 type Buffer[T any] struct {
 	_             [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
 	pullChanMutex sync.Mutex
@@ -18,13 +27,7 @@ type Buffer[T any] struct {
 	_             [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
 	pullChan      chan []T
 	_             [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
-	writePos      atomic.Uint64
-	_             [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
-	nWritten      atomic.Uint64
-	//_        [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
-	//bufsize  int
-	_    [CacheLinePadding]byte // makes it 2-3x faster :D, due to prevention of false sharing
-	data []T
+	currentBatch  atomic.Pointer[proccessingBatch[T]]
 }
 
 type flushingBatch[T any] struct {
@@ -47,7 +50,6 @@ func NewBuffer[T any](batchSize int, queueSize int) *Buffer[T] {
 	res := &Buffer[T]{
 		pushChan: make(chan flushingBatch[T], totalBufCount),
 		pullChan: make(chan []T, totalBufCount),
-		data:     make([]T, batchSize),
 	}
 
 	// add totalBufCount to pullChan
@@ -83,31 +85,35 @@ func (b *Buffer[T]) PushOne(item T) {
 
 tryAgain:
 
-	if b.data == nil {
+	var batch *proccessingBatch[T] = b.currentBatch.Load()
+	if batch == nil {
 		func() {
 			b.pullChanMutex.Lock()
 			defer b.pullChanMutex.Unlock()
-			if b.data == nil {
-				b.data = <-b.pullChan
+			batch = b.currentBatch.Load()
+			if batch == nil {
+				data := <-b.pullChan
+				batch = &proccessingBatch[T]{data: data}
+				b.currentBatch.Store(batch)
 			}
 		}()
 	}
 
-	writePos := b.writePos.Add(1) - 1
-	if int(writePos) == len(b.data) {
-		for b.nWritten.Load() != uint64(len(b.data)) {
+	writePos := batch.writePos.Add(1) - 1
+	if int(writePos) == len(batch.data) {
+		for batch.nWritten.Load() != uint64(len(batch.data)) {
 			runtime.Gosched() // must wait until all elements are written, i.e. all other goroutines have finished writing
 		}
-		b.pushChan <- flushingBatch[T]{size: len(b.data), buf: b.data}
-		b.data = nil
-		b.nWritten.Store(0)
-		b.writePos.Store(0)
+		b.pushChan <- flushingBatch[T]{size: len(batch.data), buf: batch.data}
+		b.currentBatch.Store(nil)
+		batch.nWritten.Store(0)
+		batch.writePos.Store(0)
 		goto tryAgain
-	} else if int(writePos) > len(b.data) {
+	} else if int(writePos) > len(batch.data) {
 		runtime.Gosched()
 		goto tryAgain
 	}
 
-	b.data[writePos] = item
-	b.nWritten.Add(1)
+	batch.data[writePos] = item
+	batch.nWritten.Add(1)
 }
